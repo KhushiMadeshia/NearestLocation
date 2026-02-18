f8e94cc7c1b1c812134a58bced760cc4efbcf4dd
khushimadeshia-dev <khushi.madeshia@saasbanana.com>
2026-02-18 22:32:00 +0530

    1

---
+++ app/src/main/java/com/example/nearestlocation/ui/HomeActivity.java
@@ -0,0 +1,6 @@
+package com.example.nearestlocation.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/nearestlocation/ui/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.nearestlocation.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nearestlocation.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, HomeActivity.class));
+            finish();
+        }, 2000);
+    }
+}
+
