60935cd831845fe2e569ed6ebefff79a7d00acc3
khushimadeshia-dev <khushi.madeshia@saasbanana.com>
2026-02-19 11:05:57 +0530

    2

--- app/src/main/java/com/example/nearestlocation/ui/HomeActivity.java
+++ app/src/main/java/com/example/nearestlocation/ui/HomeActivity.java
@@ -1,6 +1,22 @@
 package com.example.nearestlocation.ui;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nearestlocation.R;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        double lat = getIntent().getDoubleExtra("LATITUDE", 0.0);
+        double lng = getIntent().getDoubleExtra("LONGITUDE", 0.0);
+
+        Log.d("HOME_LOCATION", "Lat: " + lat + ", Lng: " + lng);
+    }
 }
--- app/src/main/java/com/example/nearestlocation/ui/SplashActivity.java
+++ app/src/main/java/com/example/nearestlocation/ui/SplashActivity.java
@@ -1,25 +1,133 @@
 package com.example.nearestlocation.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.nearestlocation.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST = 100;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private double latitude = 0.0;
+    private double longitude = 0.0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        checkLocationPermission();
+    }
+
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST
+            );
+
+        } else {
+
+            getCurrentLocation();
+
+        }
+    }
+
+    private void goToNextScreen() {
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            startActivity(new Intent(this, HomeActivity.class));
+
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra("LATITUDE", latitude);
+            intent.putExtra("LONGITUDE", longitude);
+
+            startActivity(intent);
             finish();
+
         }, 2000);
     }
-}
 
+
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                getCurrentLocation();
+
+
+            }else {
+                Toast.makeText(
+                        this,
+                        "Location permission is required for best experience",
+                        Toast.LENGTH_LONG
+                ).show();
+
+                goToNextScreen();
+            }
+
+        }
+    }
+
+    private void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            goToNextScreen();
+            return;
+        }
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(location -> {
+
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+
+                        Log.d("LOCATION",
+                                "Lat: " + latitude + ", Lng: " + longitude);
+                    }
+                    else {
+                        Log.d("LOCATION", "Location is null");
+                    }
+
+                    goToNextScreen();
+                });
+    }
+
+
+}
